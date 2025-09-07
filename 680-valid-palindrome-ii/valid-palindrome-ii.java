a97cdd8b2834474697450bfb751344bd62162b80
Gunjankumar Choudhari <115869342+Gunjankumar55@users.noreply.github.com>
2025-09-07 17:23:49 +0530

    Time: 4 ms (99.62%) | Memory: 45.5 MB (63.02%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public static boolean ispal(String s , int left , int right){
+        while(left < right){
+            if(s.charAt(left) !=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+    public boolean validPalindrome(String s) {
+        int left = 0; 
+        int right = s.length() -1 ;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right))
+            {
+                return ispal(s,left+1 ,right) || ispal(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
