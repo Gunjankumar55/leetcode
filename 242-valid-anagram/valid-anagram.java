c31ddb2c1be6eb67ee1ced7547c8792d3c9e6a2f
Gunjan C. <115869342+Gunjankumar55@users.noreply.github.com>
2026-08-17 16:17:23 +0530

    Time: 4 ms (83.02%) | Memory: 46.8 MB (9.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,22 @@
-class Solution 
-{
-    
+class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
+        boolean isana = true;
         char [] c1 = s.toCharArray();
         char [] c2 = t.toCharArray();
-
         Arrays.sort(c1);
         Arrays.sort(c2);
 
-        return Arrays.equals(c1,c2);
+        if(c1.length != c2.length){
+            return false;
+        }
+        for(int i = 0 ; i < c1.length ; i++)
+        {
+            if(c1[i] != c2[i]){
+                isana = false;
+            }
 
+        }
+        return isana;
 
     }
 }
